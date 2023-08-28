87390e1c462452a92a0c5412345270b1b7b79507
unknown <jonas.gastro91@gmail.com>
2023-08-28 14:34:19 -0300

    migrations schedules

---
+++ API_schedule/src/migrations/20230828172625_create-table-schedules.js
@@ -0,0 +1,17 @@
+const { BaseRepository } = require("@cubos/knex-repository")
+
+const Knex = require("knex")
+
+exports.up = async function (knex) {
+  await knex.schema.createTable('schedules', (table)=> {
+    table.uuid('id').primary().notNullable();
+
+    table.date("date").notNullable();
+    table.time("start").notNullable();
+    table.time("end").notNullable();
+  })
+};
+
+exports.down = async function (knex) {
+    await BaseRepository.dropTable(knex, "schedules");
+};
