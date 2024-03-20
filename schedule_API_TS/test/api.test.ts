3f6bbdf27c8af93f4e3c65ec9b888c5da85b42e7
unknown <jonas.gastro91@gmail.com>
2024-03-20 15:35:47 -0300

    Create client new api clean TDD

---
+++ schedule_API_TS/test/api.test.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+test("Deve cadastrar um cliente", async function () {
+	const input = {
+		name: "John Doe",
+		email: "john.doe@gmail.com",
+		phone: "83432616074",
+    password: "123456",
+	};
+	const response1 = await axios.post("http://localhost:3000/client", input);
+	const output1 = response1.data;
+	expect(output1).toBeDefined();
+});
